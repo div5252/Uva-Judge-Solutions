90f2b42c275364682cc600590313cf9b6736cc31
div5252 <54735797+div5252@users.noreply.github.com>
2020-04-07 19:55:19 +0530

    Create 10004 - Bicoloring.cpp

---
+++ 10004 - Bicoloring.cpp
@@ -0,0 +1,41 @@
+//Using bipartite graph. Time-0.000s
+
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+#define vll vector<ll>
+
+int main()
+{
+    ll n,l,a,b;
+    while(cin>>n)
+    {
+        if(n==0) break;
+        cin>>l;
+        vll AdjList[n];
+        ll color[n];
+        for(int i=0;i<l;i++)
+        {
+            cin>>a>>b;
+            AdjList[a].push_back(b);
+        }
+        queue<int> q; q.push(0);
+        for(int i=0;i<n;i++)
+        {
+            color[i]=-1;
+        }
+        color[0] = 0;
+        bool isBipartite = true; // addition of one boolean flag, initially true
+        while (!q.empty() && isBipartite) { // similar to the original BFS routine
+            int u = q.front(); q.pop();
+            for (int j = 0; j < (int)AdjList[u].size(); j++) {
+                if (color[AdjList[u][j]] == -1) { // but, instead of recording distance,
+                    color[AdjList[u][j]] = 1 - color[u]; // we just record two colors {0, 1}
+                    q.push(AdjList[u][j]); }
+                else if (color[AdjList[u][j]] == color[u]) { // u & v.first has same color
+                    isBipartite = false; break; } } } // we have a coloring conflict
+        if(isBipartite) cout<<"BICOLORABLE.\n";
+        else cout<<"NOT BICOLORABLE.\n";
+    }
+}
