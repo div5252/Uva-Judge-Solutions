bbc71c4a5069b6a3e88f4c49b908798299deedb5
div5252 <54735797+div5252@users.noreply.github.com>
2020-06-02 10:55:59 +0530

    Create 11475 - Extend to Palindrome.cpp

---
+++ 11475 - Extend to Palindrome.cpp
@@ -0,0 +1,62 @@
+//Using KMP. Time-0.030s
+
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+#define vll vector<ll>
+#define ld long double
+
+char T[200005], P[200005]; // T = text, P = pattern
+int b[200005], n, m; // b = back table, n = length of T, m = length of P
+
+void kmpPreprocess() { // call this before calling kmpSearch()
+    int i = 0, j = -1; b[0] = -1; // starting values
+    while (i < m) { // pre-process the pattern string P
+        while (j >= 0 && P[i] != P[j]) j = b[j]; // different, reset j using b
+        i++; j++; // if same, advance both pointers
+        b[i] = j; // observe i = 8, 9, 10, 11, 12, 13 with j = 0, 1, 2, 3, 4, 5
+} } // in the example of P = "SEVENTY SEVEN" above
+
+int kmpSearch() { // this is similar as kmpPreprocess(), but on string T
+    int i = 0, j = 0; // starting values
+    while (i < n) { // search through string T
+        while (j >= 0 && T[i] != P[j]) j = b[j]; // different, reset j using b
+        i++; j++; // if same, advance both pointers
+        if (j == m) { // a match found when j == m
+            //printf("P is found at index %d in T\n", i - j);
+            //j = b[j]; // prepare j for the next possible match
+        }
+    } 
+    return j;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    string s;
+    while(cin>>s)
+    {
+        for(int i=0;i<s.size();i++)
+        {
+            T[i]=s[i];
+            P[i]=s[s.size()-i-1];
+        }
+        n=s.size();
+        m=s.size();
+        kmpPreprocess();
+        int tail=kmpSearch();
+        //cout<<tail<<" ";
+        for(int i=0;i<s.size()-tail;i++)
+        {
+            cout<<T[i];
+        }
+        for(int i=0;i<s.size();i++)
+        {
+            cout<<P[i];
+        }
+        cout<<"\n";
+    }
+}
